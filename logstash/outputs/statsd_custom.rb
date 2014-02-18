33983d0433af9cade04e8177d499dc30927b1f1d
Jiri Barton <jbar@hosting4u.cz>
2014-02-18 19:03:26 +0100

    added namespace to the statsd output

--- logstash/outputs/statsd_custom.rb
+++ logstash/outputs/statsd_custom.rb
@@ -11,13 +11,22 @@ class LogStash::Outputs::StatsdCustom < LogStash::Outputs::UDP
   # The fields to monitor for statsd packets.
   config :fields, :validate => :array, :default => ["message"]
   
+  # The prefix to prepend to the extracted metrics
+  config :namespace, :validate => :string, :default => nil
+
+  public
+  def register
+    super
+    @prefix = @namespace ? "%s." % @namespace : ""
+  end
+
   public
   def receive(event)
     return unless output?(event)
 
     @fields.each do |field|    
       event[field].split(/\s+/).each do |token|
-        @codec.encode(token) if token =~ /[a-zA-Z0-9_.-]+:[a-zA-Z0-9|:_.-]+/
+        @codec.encode(@prefix + token) if token =~ /[a-zA-Z0-9_.-]+:[a-zA-Z0-9|:_.-]+/
       end
     end
   end
